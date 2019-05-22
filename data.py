448e1fe8f2d635c3f78e36197718bc35618fa8de
hyk2018 <43549187+hyk2018@users.noreply.github.com>
2019-05-22 00:39:16 -0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,36 @@
+import psycopg2
+conn=psycopg2.connect(
+    database="myfirst",
+    user="hyk",
+    password="123456",
+    host="localhost",
+    port="5432"
+)
+
+print("connected to postgres")
+
+#format: conn = psycopg2.connect(dbname="postgres", user="",password="", host="127.0.0.1", port="5432")
+
+cur = conn.cursor()
+
+#C
+cur.execute("insert into cities(name,location) values('CZ','(120,110)')")
+#R
+cur.execute("select * from cities")
+#U
+cur.execute("update cities set name='Changzhou' where name='CZ'")
+#D
+cur.execute("delete from cities where name='Changzhou'")
+
+
+
+rows = cur.fetchall()
+
+
+print('//////////////////////////')
+for row in rows:
+    print('name=' + str(row[0]) + ' location=' + str(row[1]))
+print('//////////////////////////\n')
+
+conn.commit()
+conn.close()
